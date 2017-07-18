6448d314689c21d808ca65763815b9569390ea13
johndoe <nicoaudy1994@gmail.com>
2017-07-18 15:19:09 +0700

    updating profile information

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace Chatty\Http\Controllers;
 
 use Chatty\Http\Controllers\Controller;
 use Chatty\Models\User;
+use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {	
@@ -14,5 +15,27 @@ class ProfileController extends Controller
 			abort(404);
 		}
 		return view('profile.index', compact('user'));
+	}
+
+	public function getEdit()
+	{
+		return view('profile.edit');
+	}
+
+	public function postEdit(Request $request)
+	{
+		$this->validate($request, [
+			'first_name'	=> 'alpha|max:50',
+			'last_name'		=> 'alpha|max:50',
+			'location'		=> 'max:20',
+		]);
+
+		auth()->user()->update([
+			'first_name'	=> $request->first_name,
+			'last_name'		=> $request->last_name,
+			'location'		=> $request->location,
+		]);
+
+		return redirect()->route('profile.edit')->with('info', 'Your profile has been updated.');
 	}
 }
