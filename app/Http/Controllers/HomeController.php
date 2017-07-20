873747b7eb060da09bcb7fb9d06e168f2bbd32af
johndoe <nicoaudy1994@gmail.com>
2017-07-20 14:44:54 +0700

    Showing statuses on the timeline

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,19 @@
 namespace Chatty\Http\Controllers;
 
 use Chatty\Http\Controllers\Controller;
+use Chatty\Models\Status;
 
 class HomeController extends Controller
 {	
 	public function index()
 	{
 		if (auth()->check()) {
-			return view('timeline.index');
+			$statuses = Status::where(function($query){
+				return $query->where('user_id', auth()->user()->id)->orWhere('user_id', auth()->user()->friends()->pluck('id'));
+			})
+			->orderBy('created_at', 'desc')
+			->paginate(10);
+			return view('timeline.index', compact('statuses'));
 		}
 		return view('home');
 	}
