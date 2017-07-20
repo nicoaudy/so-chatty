9a7febc81d3b4fc93f084473f413581db9f3abb5
johndoe <nicoaudy1994@gmail.com>
2017-07-20 11:41:59 +0700

    friend model methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace Chatty\Models;
 
-use Illuminate\Notifications\Notifiable;
+use Chatty\Models\User;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -66,5 +67,36 @@ class User extends Authenticatable
     public function friendRequests()
     {
         return $this->friendsOfMine()->wherePivot('accepted', false)->get();
+    }
+
+    public function friendRequestsPending()
+    {
+        return $this->friendOf()->wherePivot('accepted', false)->get();
+    }
+
+    public function hasFriendRequestPending(User $user)
+    {
+        return (bool) $this->friendRequestsPending()->where('id', $user->id)->count();
+    }
+
+    public function hasFriendRequestRecieved(User $user)
+    {
+        return (bool) $this->friendRequests()->where('id', $user->id)->count();
+    }
+    public function addFriend(User $user)
+    {
+        return $this->friendOf()->attach($user->id);
+    }
+
+    public function acceptFriendRequest(User $user)
+    {
+        return $this->friendRequests()->where('id', $user->id)->first()->pivot->update([
+            'accepted' => true
+        ]);
+    }
+
+    public function isFriendsWith(User $user)
+    {
+        return (bool) $this->friends()->where('id', $user->id)->count();
     }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,6 +6,16 @@
         @include('user.partials.userblock')<hr>
     </div>
     <div class="col-lg-4 col-lg-offset-3">
+        @if(auth()->user()->hasFriendRequestPending($user))
+            <p>waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
+        @elseif(auth()->user()->hasFriendRequestRecieved($user))
+            <a href="" class="btn btn-primary">Accept friend request</a>
+        @elseif(auth()->user()->isFriendsWith($user))
+            <p>You and {{ $user->getNameOrUsername() }} are friends</p>
+        @else
+            <a href="" class="btn btn-primary">Add as friends</a>
+        @endif
+
         <h4>{{ $user->getFirstNameOrUsername() }}'s Friends.</h4>
         @if(!$user->friends()->count())
             <p>{{ $user->getFirstNameOrUsername() }} has no friends.</p>
